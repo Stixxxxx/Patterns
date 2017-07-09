b643dbc3d822f77fc88f3a9b894bd3471e90473c
Stixxxxx <Stixxx@inbox.ru>
2017-07-09 22:22:02 +0300

    ScanFiles

---
+++ VisualStudioProjects/ScanFiles/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string path = "D:\\";
+        
+        string[] fullfilesPath;
+
+        string[] fulldirPath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+                
+
+        public void ReLoad(string path)
+        {
+
+            //try
+            //{ 
+
+            //var directories = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+
+            //foreach (var d in directories)
+            //{
+            //    lbFilDir.Items.Add(d);
+            //}
+
+            //}
+
+            //catch
+            //{
+
+            //}
+
+
+            //fullfilesPath = Directory.GetFiles(path, "*.*");
+            //fulldirPath = Directory.GetDirectories(path);
+
+            //for (int ii = 0; ii < fullfilesPath.Length; ii++)
+
+            //{ 
+
+            //    for (int i = 0; i < fullfilesPath.Length; i++)
+            //{ 
+            //    lbFilDir.Items.Add(fullfilesPath[i]);
+
+            //}
+
+            //for (int i = 0; i < fulldirPath.Length; i++)
+            //{
+
+            //    ReLoad(fulldirPath[0]);
+
+            //}
+
+            //    ReLoad(fulldirPath+"\\..");
+
+            //}
+
+            IEnumerable<string> a = GetFileList("*.*", path);
+
+
+        }
+
+
+        public static IEnumerable<string> GetFileList(string fileSearchPattern, string rootFolderPath)
+        {
+            Queue<string> pending = new Queue<string>();
+            pending.Enqueue(rootFolderPath);
+            string[] tmp;
+            while (pending.Count > 0)
+            {
+                rootFolderPath = pending.Dequeue();
+                try
+                {
+                    tmp = Directory.GetFiles(rootFolderPath, fileSearchPattern);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    continue;
+                }
+                for (int i = 0; i < tmp.Length; i++)
+                {
+                    yield return tmp[i];
+                }
+                tmp = Directory.GetDirectories(rootFolderPath);
+                for (int i = 0; i < tmp.Length; i++)
+                {
+                    pending.Enqueue(tmp[i]);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+          ReLoad(path);
+
+        }
+    }
+
+}
---
+++ VisualStudioProjects/ScanFiles/WpfApp4/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CancellationTokenSource CancellationToken;
+        Task ScanTask;
+        public MainWindow()
+        {
+            InitializeComponent();
+         
+
+        }
+
+        public string ReLoad(string path)
+        {
+            var directories = new List<DirectoryInfo> { new DirectoryInfo(path) };
+
+            while (directories.Count > 0)
+            {
+                if (CancellationToken.IsCancellationRequested)
+                    return "cancel";
+
+                var curent = directories.First();
+                try
+                {
+                    directories.AddRange(curent.GetDirectories());
+                    curent
+                        .GetFiles()
+                        .ToList()
+                        .ForEach(x => Dispatcher.Invoke(() => lbFilDir.Items.Add(x.FullName)));
+                }
+                catch { }
+                finally
+                {
+                    directories.Remove(curent);
+                }
+            }
+
+            return "good";
+        }
+
+        void Button_Click_Stop(object sender, RoutedEventArgs e)
+        {
+            CancellationToken.Cancel();
+        }
+
+        private void Button_Click_Go(object sender, RoutedEventArgs e)
+        {
+            
+            CancellationToken = new CancellationTokenSource();
+            ScanTask = new Task(() => ReLoad("c:"), CancellationToken.Token);
+            Loaded += (s, a) => ScanTask.Start();
+            
+        }
+    }
+}
